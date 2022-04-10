6c89f805544cc519d1f3f8041069a6b1dc4f4f8f
Zachary Dowd <zacharyd2796@zohomail.com>
2022-04-10 15:07:30 -0600

    added 'axe'

--- axe_game.cpp
+++ axe_game.cpp
@@ -2,13 +2,17 @@
 int main()
 {
     // window dimensions
-    int width{350};
+    int width{800};
     int height{500};
     InitWindow(width, height, "Zachary's Window");
 
     // circle coordinates
-    int circle_x{175};
-    int circle_y{250};
+    int circle_x{200};
+    int circle_y{200};
+
+    // axe coordinates
+    int axe_x{400};
+    int axe_y{0};
 
     // runs game
     SetTargetFPS(60);
@@ -19,13 +23,18 @@ int main()
 
         // Game logic begins
         DrawCircle(circle_x, circle_y, 25, BLUE);
-        if (IsKeyDown(KEY_D))
+        DrawRectangle(axe_x, axe_y, 50, 50, RED);
+
+        // Move the axe
+        axe_y += 10;
+
+        if (IsKeyDown(KEY_D) && circle_x < 350)
         {
-            circle_x = circle_x + 10;
+            circle_x += 10;
         }
-        if (IsKeyDown(KEY_A))
+        if (IsKeyDown(KEY_A) && circle_x > 0)
         {
-            circle_x = circle_x - 10;
+            circle_x -= 10;
         }
 
         // Game logic ends
